119c831dc86534186c37c61536e0ff76066f68af
Ola Oyo <ola.oyo@gmail.com>
2022-11-30 20:54:18 +0100

    Starting files

---
+++ meubel_house/meubel_house/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from meubel_house.users.models import User
+from meubel_house.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
