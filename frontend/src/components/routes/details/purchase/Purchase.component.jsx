1983d8951ab159857be08ccd42aded848cd175f3
Ola Oyo <ola.oyo@gmail.com>
2022-12-24 11:24:15 +0100

    Purchase details increase & decrease quantity

--- frontend/src/components/routes/details/purchase/Purchase.component.jsx
+++ frontend/src/components/routes/details/purchase/Purchase.component.jsx
@@ -37,7 +37,7 @@ import Message from "../../../message/Message.component";
 import { APILink } from "../../../../api/api";
 
 function Purchase({ loading, furniture, error, furnitureId }) {
-  const [amount, setAmount] = useState(1);
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
 
@@ -49,21 +49,33 @@ function Purchase({ loading, furniture, error, furnitureId }) {
   const navigate = useNavigate();
 
   const addFurniture = () => {
-    setAmount(amount + 1);
+    setQty(qty + 1);
   };
 
   const removeFurniture = () => {
-    setAmount(amount > 1 ? amount - 1 : amount);
+    setQty(qty > 1 ? qty - 1 : qty);
   };
 
   const onChangeHandler = (event) => {
-    setAmount(event.target.value);
+    setQty(event.target.value);
   };
 
+  const increaseFurnitureQtyHandler = (furnitureId) => {
+    dispatch(increaseFurnitureQty(furnitureId))
+  }
+
+  const decreaseFurnitureQtyHandler = (furnitureId) => {
+    dispatch(decreaseFurnitureQty(furnitureId))
+  }
+
   const addToCartHandler = () => {
-    dispatch(addFurnitureToCart(furnitureId, amount))
+    dispatch(addFurnitureToCart(furnitureId, qty))
     navigate(APILink.cart);
-    // navigate(APILink.addToCart(furnitureId, amount));
+  };
+
+  const addToCartItemsHandler = () => {
+    dispatch(addFurnitureToCart(furnitureId, currentFurniture.qty))
+    navigate(APILink.cart);
   };
 
   return (
@@ -72,106 +84,206 @@ function Purchase({ loading, furniture, error, furnitureId }) {
         <Loading />
       ) : error ? (
         <Message>{error}</Message>
-      ) : (
+      ) : cartItems.length && currentFurniture ? (
+        // Qty from localStorage cartItems
         <PurchaseStyles>
-          <ImgSmallGrid>
-            <ImgSmall src={furniture.image} alt={furniture.name} />
-            <ImgSmall src={furniture.image} alt={furniture.name} />
-            <ImgSmall src={furniture.image} alt={furniture.name} />
-            <ImgSmall src={furniture.image} alt={furniture.name} />
-          </ImgSmallGrid>
-          <ImgBigGrid>
-            <ImgBig src={furniture.image} alt={furniture.name} />
-          </ImgBigGrid>
-          <DetailsGrid>
-            <DetailsHeader>{furniture.name}</DetailsHeader>
-            <Details p1 grey>
-              ${furniture.price}
-            </Details>
-            <Reviews>
-              <RatingGrid>
-                <Rating rating={furniture.rating} />
-              </RatingGrid>
-              <ReviewsButton>
-                <Details p6 grey>
-                  {furniture.numReviews} Customer Reviews
-                </Details>
-              </ReviewsButton>
-            </Reviews>
-            <Description>
-              <Details p7>{furniture.description}</Details>
-            </Description>
-            <SizeAndColorGrid>
-              <Details grey>Size</Details>
-              <SizeAndColor>
-                <SizeBg selected>
-                  <Details>L</Details>
-                </SizeBg>
-                <SizeBg>
-                  <Details>XL</Details>
-                </SizeBg>
-                <SizeBg>
-                  <Details>XS</Details>
-                </SizeBg>
-              </SizeAndColor>
-              <Details grey>Color</Details>
-              <SizeAndColor>
-                <ColorBg purple />
-                <ColorBg black />
-                <ColorBg gold />
-              </SizeAndColor>
-            </SizeAndColorGrid>
-            <QtyAddToCart>
-              <Qty>
-                <AddRemove onClick={removeFurniture}>
-                  <span className="material-symbols-outlined">remove</span>
-                </AddRemove>
-                <Details p3 value={amount} onChange={onChangeHandler}>
-                  {/* {currentFurniture ? currentFurniture.qty : amount} */}
-                  {amount}
-                </Details>
-                <AddRemove onClick={addFurniture}>
-                  <span className="material-symbols-outlined">add</span>
-                </AddRemove>
-              </Qty>
-              <AddToCartButton onClick={addToCartHandler}>
-                Add To Cart
-              </AddToCartButton>
-            </QtyAddToCart>
-            <Tags>
-              <Tag>
-                <Details grey>SKL</Details>
-                <Details grey ml>
-                  : SS001
-                </Details>
-              </Tag>
-              <Tag>
-                <Details grey>Category</Details>
-                <Details grey ml>
-                  : Sofas
-                </Details>
-              </Tag>
-              <Tag>
-                <Details grey>Tags</Details>
-                <Details grey ml>
-                  : {furniture.category}
-                </Details>
-              </Tag>
-              <Tag>
-                <Details grey>Share</Details>
-                <SocialMedia grey ml>
-                  :
-                  <Social>
-                    <span className="material-symbols-outlined">thumb_up</span>
-                  </Social>
-                  <Social>
-                    <span className="material-symbols-outlined">share</span>
-                  </Social>
-                </SocialMedia>
-              </Tag>
-            </Tags>
-          </DetailsGrid>
-        </PurchaseStyles>
+        <ImgSmallGrid>
+          <ImgSmall src={furniture.image} alt={furniture.name} />
+          <ImgSmall src={furniture.image} alt={furniture.name} />
+          <ImgSmall src={furniture.image} alt={furniture.name} />
+          <ImgSmall src={furniture.image} alt={furniture.name} />
+        </ImgSmallGrid>
+        <ImgBigGrid>
+          <ImgBig src={furniture.image} alt={furniture.name} />
+        </ImgBigGrid>
+        <DetailsGrid>
+          <DetailsHeader>{furniture.name}</DetailsHeader>
+          <Details p1 grey>
+            ${furniture.price}
+          </Details>
+          <Reviews>
+            <RatingGrid>
+              <Rating rating={furniture.rating} />
+            </RatingGrid>
+            <ReviewsButton>
+              <Details p6 grey>
+                {furniture.numReviews} Customer Reviews
+              </Details>
+            </ReviewsButton>
+          </Reviews>
+          <Description>
+            <Details p7>{furniture.description}</Details>
+          </Description>
+          <SizeAndColorGrid>
+            <Details grey>Size</Details>
+            <SizeAndColor>
+              <SizeBg selected>
+                <Details>L</Details>
+              </SizeBg>
+              <SizeBg>
+                <Details>XL</Details>
+              </SizeBg>
+              <SizeBg>
+                <Details>XS</Details>
+              </SizeBg>
+            </SizeAndColor>
+            <Details grey>Color</Details>
+            <SizeAndColor>
+              <ColorBg purple />
+              <ColorBg black />
+              <ColorBg gold />
+            </SizeAndColor>
+          </SizeAndColorGrid>
+          <QtyAddToCart>
+            <Qty>
+              <AddRemove onClick={() => decreaseFurnitureQtyHandler(currentFurniture.id)}>
+                <span className="material-symbols-outlined">remove</span>
+              </AddRemove>
+              <Details p3 value={currentFurniture.qty} onChange={onChangeHandler}>
+                {currentFurniture.qty}
+              </Details>
+              <AddRemove onClick={() => increaseFurnitureQtyHandler(currentFurniture.id)}>
+                <span className="material-symbols-outlined">add</span>
+              </AddRemove>
+            </Qty>
+            <AddToCartButton onClick={addToCartItemsHandler}>
+              Add To Cart
+            </AddToCartButton>
+          </QtyAddToCart>
+          <Tags>
+            <Tag>
+              <Details grey>SKL</Details>
+              <Details grey ml>
+                : SS001
+              </Details>
+            </Tag>
+            <Tag>
+              <Details grey>Category</Details>
+              <Details grey ml>
+                : Sofas
+              </Details>
+            </Tag>
+            <Tag>
+              <Details grey>Tags</Details>
+              <Details grey ml>
+                : {furniture.category}
+              </Details>
+            </Tag>
+            <Tag>
+              <Details grey>Share</Details>
+              <SocialMedia grey ml>
+                :
+                <Social>
+                  <span className="material-symbols-outlined">thumb_up</span>
+                </Social>
+                <Social>
+                  <span className="material-symbols-outlined">share</span>
+                </Social>
+              </SocialMedia>
+            </Tag>
+          </Tags>
+        </DetailsGrid>
+        </PurchaseStyles>       
+      ) : (
+        // Qty from useState()
+        <PurchaseStyles>
+        <ImgSmallGrid>
+          <ImgSmall src={furniture.image} alt={furniture.name} />
+          <ImgSmall src={furniture.image} alt={furniture.name} />
+          <ImgSmall src={furniture.image} alt={furniture.name} />
+          <ImgSmall src={furniture.image} alt={furniture.name} />
+        </ImgSmallGrid>
+        <ImgBigGrid>
+          <ImgBig src={furniture.image} alt={furniture.name} />
+        </ImgBigGrid>
+        <DetailsGrid>
+          <DetailsHeader>{furniture.name}</DetailsHeader>
+          <Details p1 grey>
+            ${furniture.price}
+          </Details>
+          <Reviews>
+            <RatingGrid>
+              <Rating rating={furniture.rating} />
+            </RatingGrid>
+            <ReviewsButton>
+              <Details p6 grey>
+                {furniture.numReviews} Customer Reviews
+              </Details>
+            </ReviewsButton>
+          </Reviews>
+          <Description>
+            <Details p7>{furniture.description}</Details>
+          </Description>
+          <SizeAndColorGrid>
+            <Details grey>Size</Details>
+            <SizeAndColor>
+              <SizeBg selected>
+                <Details>L</Details>
+              </SizeBg>
+              <SizeBg>
+                <Details>XL</Details>
+              </SizeBg>
+              <SizeBg>
+                <Details>XS</Details>
+              </SizeBg>
+            </SizeAndColor>
+            <Details grey>Color</Details>
+            <SizeAndColor>
+              <ColorBg purple />
+              <ColorBg black />
+              <ColorBg gold />
+            </SizeAndColor>
+          </SizeAndColorGrid>
+          <QtyAddToCart>
+            <Qty>
+              <AddRemove onClick={removeFurniture}>
+                <span className="material-symbols-outlined">remove</span>
+              </AddRemove>
+              <Details p3 value={qty} onChange={onChangeHandler}>
+                {qty}
+              </Details>
+              <AddRemove onClick={addFurniture}>
+                <span className="material-symbols-outlined">add</span>
+              </AddRemove>
+            </Qty>
+            <AddToCartButton onClick={addToCartHandler}>
+              Add To Cart
+            </AddToCartButton>
+          </QtyAddToCart>
+          <Tags>
+            <Tag>
+              <Details grey>SKL</Details>
+              <Details grey ml>
+                : SS001
+              </Details>
+            </Tag>
+            <Tag>
+              <Details grey>Category</Details>
+              <Details grey ml>
+                : Sofas
+              </Details>
+            </Tag>
+            <Tag>
+              <Details grey>Tags</Details>
+              <Details grey ml>
+                : {furniture.category}
+              </Details>
+            </Tag>
+            <Tag>
+              <Details grey>Share</Details>
+              <SocialMedia grey ml>
+                :
+                <Social>
+                  <span className="material-symbols-outlined">thumb_up</span>
+                </Social>
+                <Social>
+                  <span className="material-symbols-outlined">share</span>
+                </Social>
+              </SocialMedia>
+            </Tag>
+          </Tags>
+        </DetailsGrid>
+        </PurchaseStyles>         
       )}
     </>
   );
